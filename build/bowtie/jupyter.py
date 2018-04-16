4335f876f860ae5fec0b886ffda156953eb99b1c
Syed Hussain Ather <shussainather@gmail.com>
2018-04-16 11:29:31 -0400

    init commit

--- build/bowtie/jupyter.py
+++ build/bowtie/jupyter.py
@@ -1,8 +1,18 @@
 import os
 
-
 os.system(
     """
     bowtie2-build input params
     """
 )
+
+os.system(
+    """
+    bowtie2 \
+    -x params \
+    -U input.fastq  \
+    -S output.sam \
+    2> logs/bowtie-map.log && \
+    samtools view -Sbh output.sam > output && \
+    rm output.sam
+    """)
