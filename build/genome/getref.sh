d30ce9e43201933e966aa0d5a23b416d148a0938
Syed Hussain Ather <shussainather@gmail.com>
2018-04-16 09:33:16 -0400

    initial commit

---
+++ build/genome/getref.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# http://ncbiinsights.ncbi.nlm.nih.gov/2013/12/24/introducing-the-new-human-genome-assembly-grch38/
+
+# http://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.30
+# http://www.ncbi.nlm.nih.gov/genome/doc/ftpfaq/
+
+
