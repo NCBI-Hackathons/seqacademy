e59b08a1599c66451500ba83948dfddab6e10146
Syed Hussain Ather <shussainather@gmail.com>
2018-04-16 09:37:00 -0400

    removed .sh files

--- build/genome/getref.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# http://ncbiinsights.ncbi.nlm.nih.gov/2013/12/24/introducing-the-new-human-genome-assembly-grch38/
-
-# http://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.30
-# http://www.ncbi.nlm.nih.gov/genome/doc/ftpfaq/
-
-
