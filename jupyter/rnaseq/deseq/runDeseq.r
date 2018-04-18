49401804c3e5f7044d8ec0b1e2a5cd145cf34048
Ubuntu <ubuntu@ip-172-16-242-216.ec2.internal>
2018-04-18 00:20:51 +0000

    added runDEseq

---
+++ jupyter/rnaseq/deseq/runDeseq.r
@@ -0,0 +1,23 @@
+# *****install DESeq2*****
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("DESeq2")
+
+library("DESeq2")
+
+directory <- "/home/ubuntu/hussainather/omicsedu/test/"
+# filelist <- list.files(directory)
+# sampleFiles <- grep("genecount",filelist,value=TRUE)
+sampleFiles <-c("SRR5494627.genecount.txt","SRR5494628.genecount.txt","SRR5494629.genecount.txt","SRR5494630.genecount.txt","SRR5494631.genecount.txt","SRR5494632.genecount.txt")
+sampleName <- c ("Aneuploid1", "Aneuploid2", "Aneuploid3", "Euploid1", "Euploid2", "Euploid3")
+condition <- c ("Aneuploid", "Aneuploid", "Aneuploid", "Euploid", "Euploid", "Euploid")
+sampleTable <- data.frame(sampleName=sampleName, fileName=sampleFiles, condition=condition)
+
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory=directory, design=~condition)
+ddsHTSeq <- ddsHTSeq [rowSums(counts(ddsHTSeq))>1,]
+ddsHTSeq$condition <- relevel(ddsHTSeq$condition, ref = "Euploid")
+ddsHTSeq <- DESeq(ddsHTSeq)
+res <- results(ddsHTSeq)
+write.csv (as.data.frame(res),file="TreatmentVsMock_deseq2_results.csv")
+vsd <- varianceStabilizingTransformation(ddsHTSeq, blind=FALSE)
+write.csv(assay(vsd),file="TreatmentVsMock_VSD.csv")
+
