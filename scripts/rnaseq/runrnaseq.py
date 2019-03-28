3329d120c81b3cd04f380c11d58fdff953847eb8
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 02:50:44 -0400

    combined count

--- scripts/rnaseq/runrnaseq.py
+++ scripts/rnaseq/runrnaseq.py
@@ -16,3 +16,15 @@ for index, individual in enumerate(RNASeqoutrun):
     sam = RNASeqoutputSam[index]
     bam = RNASeqoutputSortBam[index]
     os.system("hisat2 -u 2000 -x yeast_index/genome --sra-acc {0} --new-summary --summary-file {1} --met-file {2} -S {3}".format(run, summary, metrics, sam))
+
+gtf = "data/Saccharomyces_cerevisiae.R64-1-1.84.gtf"
+
+RNASeqSRARunTableFile='data/RNASeqSRA.tsv'
+RNASeqSRATable = read_csv(RNASeqSRARunTableFile, delimiter='\t')
+RNASeqoutrun = (RNASeqSRATable["Run"])
+RNASeqoutputSortBam = "test/" + RNASeqoutrun + ".sorted.bam"
+
+for index, individual in enumerate(RNASeqoutputSortBam):
+    input = individual
+    output = individual + ".genecount.txt"
+    os.system("htseq-count -m intersection-nonempty -s no -f bam %s %s > %s" % (input, gtf, output))
