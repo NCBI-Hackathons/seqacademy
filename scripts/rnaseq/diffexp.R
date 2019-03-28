48e618b726587f900927dbf8cb223170253cc599
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 04:25:44 -0400

    load gdc

--- scripts/rnaseq/diffexp.R
+++ scripts/rnaseq/diffexp.R
@@ -85,7 +85,9 @@ summary(prcomp( t(log2(filtYeastGeneCounts+1) ) ))
 ## Differential gene expression with DESeq2
 library("DESeq2")
 
-HTSeqcount <- readHTSeqFile(arg[1])
+fname = system.file(package="GenomicDataCommons", arg[1])
+
+HTSeqcount <- readHTSeqFile(fname)
 
 dds <- DESeqDataSetFromHTSeqCount(HTSeqCount, design=~karyotype)
 
