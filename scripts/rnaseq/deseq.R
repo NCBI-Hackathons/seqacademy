132191fd2a711ca34c408a0310197eee5eb1ff4c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 00:36:27 -0400

    clean

--- scripts/rnaseq/deseq.R
+++ scripts/rnaseq/deseq.R
@@ -1,10 +1,10 @@
 # *****install DESeq2*****
-source("https://bioconductor.org/biocLite.R")
-biocLite("DESeq2")
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("DESeq2")
 
 library("DESeq2")
 
-directory <- "../test/"
+directory <- "test"
 # filelist <- list.files(directory)
 # sampleFiles <- grep("genecount",filelist,value=TRUE)
 sampleFiles <-c("SRR5494627.genecount.txt","SRR5494628.genecount.txt","SRR5494629.genecount.txt","SRR5494630.genecount.txt","SRR5494631.genecount.txt","SRR5494632.genecount.txt")
@@ -13,7 +13,7 @@ condition <- c ("Aneuploid", "Aneuploid", "Aneuploid", "Euploid", "Euploid", "Eu
 sampleTable <- data.frame(sampleName=sampleName, fileName=sampleFiles, condition=condition)
 
 ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory=directory, design=~condition)
-ddsHTSeq <- ddsHTSeq [rowSums(counts(ddsHTSeq))>1,]
+ddsHTSeq <- ddsHTSeq[rowSums(counts(ddsHTSeq))>1,]
 ddsHTSeq$condition <- relevel(ddsHTSeq$condition, ref = "Euploid")
 ddsHTSeq <- DESeq(ddsHTSeq)
 res <- results(ddsHTSeq)
