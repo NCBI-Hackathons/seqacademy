10e225c9b9ca5598b9e04d54ca5a14f473cbf59c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 04:59:52 -0400

    yes

--- scripts/rnaseq/diffexp.R
+++ scripts/rnaseq/diffexp.R
@@ -88,7 +88,7 @@ library("GenomicDataCommons")
 
 args = commandArgs(trailingOnly = TRUE)
 
-sampleTable = data.frame(args[1], fileName = args[2])
+sampleTable = data.frame(sampleName=args[1], fileName = args[2])
 
 dds <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory=".", design=~karyotype)
 
