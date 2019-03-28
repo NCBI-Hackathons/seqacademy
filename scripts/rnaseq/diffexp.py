ede5db7d8405d62a625ede5df2947520334f3049
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 04:39:09 -0400

    back to no test

--- scripts/rnaseq/diffexp.py
+++ scripts/rnaseq/diffexp.py
@@ -7,4 +7,4 @@ Python script for calling each HTSeq count file and running diffexp.R on it.
 
 os.chdir("test")
 for file in glob.glob("*genecount.txt"):
-     os.system("Rscript ../scripts/rnaseq/diffexp.R test/%s" % file)
+     os.system("Rscript ../scripts/rnaseq/diffexp.R %s" % file)
