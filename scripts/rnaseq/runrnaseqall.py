385ae8e6b43af4d218d9164fc5bbf021d464000f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 05:19:12 -0400

    initial commit

---
+++ scripts/rnaseq/runrnaseqall.py
@@ -0,0 +1,30 @@
+from pandas import read_csv
+import os
+
+RNASeqSRARunTableFile='data/RNASeqSRA.tsv'
+RNASeqSRATable = read_csv(RNASeqSRARunTableFile, delimiter='\t')
+RNASeqoutrun = (RNASeqSRATable["Run"])
+RNASeqoutputSam = "test/" + RNASeqoutrun + ".sam"
+RNASeqoutputAlignmentSummary = "test/" + RNASeqoutrun + ".txt"
+RNASeqoutputMetrics = "test/" + RNASeqoutrun + ".metrics"
+RNASeqoutputSortBam = "test/" + RNASeqoutrun + ".sorted.bam"
+
+for index, individual in enumerate(RNASeqoutrun):
+    run = RNASeqoutrun[index]
+    summary = RNASeqoutputAlignmentSummary[index] 
+    metrics = RNASeqoutputMetrics[index]
+    sam = RNASeqoutputSam[index]
+    bam = RNASeqoutputSortBam[index]
+    os.system("hisat2 -x yeast_index/genome --sra-acc {0} --new-summary --summary-file {1} --met-file {2} -S {3}".format(run, summary, metrics, sam))
+
+gtf = "data/Saccharomyces_cerevisiae.R64-1-1.84.gtf"
+
+RNASeqSRARunTableFile='data/RNASeqSRA.tsv'
+RNASeqSRATable = read_csv(RNASeqSRARunTableFile, delimiter='\t')
+RNASeqoutrun = (RNASeqSRATable["Run"])
+RNASeqoutputSortBam = "test/" + RNASeqoutrun + ".sorted.bam"
+
+for index, individual in enumerate(RNASeqoutputSortBam):
+    input = individual
+    output = individual.replace(".sorted.bam", "") + ".genecount.txt"
+    os.system("htseq-count -m intersection-nonempty -s no -f bam %s %s > %s" % (input, gtf, output))
