7a517c44d5a02bd6714db728c1ba87a86efd6b0b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 04:28:00 -0400

    syntax

--- scripts/rnaseq/diffexp.py
+++ scripts/rnaseq/diffexp.py
@@ -6,5 +6,5 @@ Python script for calling each HTSeq count file and running diffexp.R on it.
 """
 
 os.chdir("test")
-for file in glob.glob("*genecount.txt")
-     os.system("Rscript scripts/rnaseq/diffexp.R %s" % file)
+for file in glob.glob("*genecount.txt"):
+     os.system("Rscript ../scripts/rnaseq/diffexp.R %s" % file)
