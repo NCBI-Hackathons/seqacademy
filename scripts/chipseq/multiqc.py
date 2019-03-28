e2dba1d4652fe5eea156f148b1af6486a9db8949
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 00:12:54 -0400

    run

--- scripts/chipseq/multiqc.py
+++ scripts/chipseq/multiqc.py
@@ -1,10 +1,12 @@
 from pandas import read_csv
 import os
 
-ChIPSeqSRARunTableFile='data/ChIPSeqSRA.tsv'
+ChIPSeqSRARunTableFile = "data/ChIPSeqSRA.tsv"
 ChIPSeqSRATable = read_csv(ChIPSeqSRARunTableFile, delimiter='\t')
 ChIPSeqoutrun = (ChIPSeqSRATable["Run"])
 ChIPSeqoutputSortBam = "test/" + ChIPSeqoutrun + ".sorted.bam"
 
 for run in ChIPSeqoutputSortBam:
-    os.system("multiqc {0} --quiet --outdir test/multiqc_chipseq --force".format(run))
+    cmd = "multiqc {0} --quiet --outdir test/multiqc_chipseq --force".format(run)
+    print(cmd)
+    os.system(cmd)
