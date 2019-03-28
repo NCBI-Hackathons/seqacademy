a5178e989795cb4737e195895773062d8895a645
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 04:16:00 -0400

    run each file

--- scripts/rnaseq/diffexp.py
+++ scripts/rnaseq/diffexp.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import glob
 import os
 
 """
 Python script for calling each HTSeq count file and running diffexp.R on it.
 """
 
-
+os.chdir("test")
+for file in glob.glob("*genecount.txt")
+     os.system("Rscript scripts/rnaseq/diffexp.R %s" % file)
