15fadb1cc78e649992f80104e600f853c7feb4ee
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 01:53:15 -0400

    paren paste

--- scripts/rnaseq/dupRadar.R
+++ scripts/rnaseq/dupRadar.R
@@ -9,35 +9,35 @@ sample <- basename(bam)
 dm <- analyzeDuprates(bam, gtf, stranded = 0, paired=FALSE, threads=5) 
 
 dm$mhRate <- (dm$allCountsMulti - dm$allCounts) / dm$allCountsMulti
-bitmap(paste("test/dupRadar/", sample, "multimapping_histogram.png", sep=""))
+bitmap(paste(("test/dupRadar/", sample, "multimapping_histogram.png"), sep=""))
 hist(dm$mhRate, breaks=50, main=basename(bam), xlab="Multimapping rate per gene", ylab="Frequency")
 dev.off()
 
-bitmap(paste("test/dupRadar/", sample, "_density_scatter.png", sep=""))
+bitmap(paste(("test/dupRadar/", sample, "_density_scatter.png"), sep=""))
 duprateExpDensPlot(dm, main=basename(bam))
 dev.off()
 
-bitmap(paste("test/dupRadar/", sample, "_expression_histogram.png", sep=""))
+bitmap(paste(("test/dupRadar/", sample, "_expression_histogram.png"), sep=""))
 expressionHist(dm)
 dev.off()
 
-bitmap(paste("test/dupRadar/", sample, "_expression_boxplot", sep=""))
+bitmap(paste(("test/dupRadar/", sample, "_expression_boxplot"), sep=""))
 par(mar=c(10,4,4,2)+.1)
 duprateExpBoxplot(dm, main=basename(bam))
 dev.off()
 
-bitmap(paste("test/dupRadar/", sample, "_expression_barplot.png", sep=""))
+bitmap(paste(("test/dupRadar/", sample, "_expression_barplot.png"), sep=""))
 readcountExpBoxplot(dm)
 dev.off()
 
-write.table(dm, file=paste("test/dupRadar/", sample, "_dm", sep=""), sep="\\t")
+write.table(dm, paste(("test/dupRadar/", sample, "_dm"), sep=""), sep="\\t")
 
 # The following is from
 # https://github.com/ewels/NGI-RNAseq/blob/master/bin/dupRadar.r
 fit <- duprateExpFit(DupMat=dm)
 df <- data.frame(intercept=as.numeric(fit$intercept), slope=c(fit$slope))
-cat("# dupRadar model params\\n", file=paste("test/dupRadar/", sample, "_model.txt", sep=""))
-write.table(df, file=paste(sep="", "test/dupRadar/", sample, "_model.txt"), sep="\\t", append=TRUE, row.names=FALSE)
+cat("# dupRadar model params\\n", paste("test/dupRadar/", sample, "_model.txt", sep=""))
+write.table(df, paste(("test/dupRadar/", sample, "_model.txt"), sep=""), sep="\\t", append=TRUE, row.names=FALSE)
 
 # Get numbers from dupRadar GLM
 curve_x <- sort(log10(dm$RPK))
@@ -58,6 +58,6 @@ curve_x = 10^curve_x
 # Write to file
 write.table(
   cbind(curve_x, curve_y),
-  file=paste("test/dupRadar/", sample, "_curve.txt", sep="")
+  paste(("test/dupRadar/", sample, "_curve.txt"), sep="")
   quote=FALSE, row.names=FALSE
 )
